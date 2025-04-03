d46649bfa2053a65e07a9452f182de5d8b16e00a
Nahuel Alvarez <alvareznahue@gmail.com>
2025-04-03 18:36:55 -0300

    uibuild

--- index.js
+++ index.js
@@ -87,4 +87,6 @@ app.post('/api/notes', (request, response) => {
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log('A ver si sirvieron los comandos');
+  
 })
